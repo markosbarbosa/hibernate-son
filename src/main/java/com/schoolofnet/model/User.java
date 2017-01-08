f0146be1e872205e5182d14d1ef4c36c3601ce43
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-08 00:24:08 -0200

    Configurando OneToMany

--- src/main/java/com/schoolofnet/model/User.java
+++ src/main/java/com/schoolofnet/model/User.java
@@ -2,11 +2,14 @@ package com.schoolofnet.model;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -36,7 +39,11 @@ public class User implements Serializable {
     @Column
     private Address address;
     
+    @Column
+    @ElementCollection
+    private List<Options> options;
     
+
     public Integer getId() {
         return id;
     }
@@ -84,6 +91,15 @@ public class User implements Serializable {
 
     public void setAddress(Address address) {
 	this.address = address;
+    }
+    
+    @OneToMany(mappedBy = "options")
+    public List<Options> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Options> options) {
+        this.options = options;
     }
     
 }
