ea2658cf156c798d1ebe81e3639e4334b7d5599f
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-08 01:29:58 -0200

    Testando buscas com hibernate

--- src/main/java/com/schoolofnet/hibernate/App.java
+++ src/main/java/com/schoolofnet/hibernate/App.java
@@ -1,9 +1,12 @@
 package com.schoolofnet.hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 
 import com.schoolofnet.model.Address;
 import com.schoolofnet.model.HibernateSession;
+import com.schoolofnet.model.Product;
 import com.schoolofnet.model.User;
 
 /**
@@ -14,24 +17,22 @@ public class App {
     public static void main( String[] args ) {
 	
 	Session session = HibernateSession.getSessionFactory().openSession();
+
+	Product product = new Product();
 	
-	User user = new User();
-	Address address = new Address();
-	
-	user.setUsername("mbarbosa");
-	user.setName("Marcos");
-	user.setLastName("Barbosa");
-	user.setAddress(address);
-	
-	address.setNumber(900);
-	address.setStreet("Down town");
-	address.setUser(user);
-	
-        System.out.println("Hello World!");
-        System.out.println(session);
+	product.setName("P 1");
+	product.setPrice(10.00);
         
-        System.out.println(user.getName() + " " + user.getLastName());
-        System.out.println(user.getAddress().getStreet());
-        
+//	session.save(product);
+
+	List<Product> products = session.createQuery("from Product").getResultList();
+	
+	Product p2 = session.find(Product.class, 3);
+	
+	System.out.println(products);
+	System.out.println(p2.getName());
+	
+//	session.getTransaction().commit();
+	
     }
 }
